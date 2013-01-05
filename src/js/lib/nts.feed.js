18cfe1bfb5b055a711d77d429e2bf8ecdc9ce744
Matt Richards <matt@lucidmoon.co.uk>
2013-01-05 20:49:06 +0000

    Feed Restyle

--- src/js/lib/nts.feed.js
+++ src/js/lib/nts.feed.js
@@ -6,6 +6,10 @@
         window.NTS.Feed = {
             render: function() {
                 var notes = NTS.getNotes();
+               
+                _.each(notes, function(note) {
+                    note.date = new Date(note.id);
+                });
                 var tmpl = Handlebars.compile($('#nts-tmpl-feedItem').html());
                 var html = tmpl({'feed':notes.reverse()});
 
