1187d8556442005399039228e7e1893d31ab05c2
Matt Richards <mr@salonmeister.de>
2014-09-08 16:46:33 +0200

    Refactor + implement storage.sync

--- src/js/lib/nts.content.js
+++ src/js/lib/nts.content.js
@@ -1,72 +1,77 @@
 (function (window) {
-    if (!window.NTS)
-        window.NTS = {};
+  if (!window.NTS)
+    window.NTS = {};
 
-    if (!window.NTS.Content) {
-        window.NTS.Content = {
-            resetDom: function() {
-                $('span.highlight').contents().unwrap();
-                $('a.highlight').removeClass('highlight');
-            },
-            highlight: function(selection) {
-                if (selection.selectionText) {
-                    // We've got selection text
-                    $('body').highlight(selection.selectionText);   
-                }
-                if (selection.linkUrl) {
-                    // We've got a link
-                    $('a[href="' + selection.linkUrl +'"]').addClass('highlight');
-                }           
-            },
-            lookup: function() {
-                chrome.extension.sendMessage({type:"lookup",url:window.location.href}, function (response) {
-                    for(var i = 0; i < response.length; i++) {
-                        NTS.Content.highlight(response[i]);
-                    }
-                }); 
-            },
-            getSelectionText: function() {
-                var html = "";
-                
-                var sel = window.getSelection();
-                if (sel.rangeCount) {
-                    var container = document.createElement("div");
-                    for (var i = 0, len = sel.rangeCount; i < len; ++i) {
-                        container.appendChild(sel.getRangeAt(i).cloneContents());
-                    }
-                    
-                }
-                
-                return container.innerText ? container.innerText : null;
-            },
-            setupKeyboardEventHandlers: function() {
-                var note, selection;
-                // Basic Keyboard Shortcut Implementation [Alt-C]
-                $(window).keyup(function(e) {
-                    if (e.altKey && e.keyCode === 67) {
-                        selection = NTS.Content.getSelectionText();
-                        note = {
-                            pageUrl: location.href,
-                            context: 'context-page',
-                            title: $('title').text()
-                        }
-
-                        if (selection) {
-                            note.selectionText = selection;
-                            note.context = 'context-selection';
-                        }
-
-                        chrome.extension.sendMessage({
-                                type:'createNote', 
-                                note: note
-                        }, function (response) {
-                            if (response.type === 'success') {
-                                chrome.extension.sendMessage({'type':'updateUI'});
-                            }
-                        });
-                    }
-                });
+  if (!window.NTS.Content) {
+    window.NTS.Content = {
+      resetDom: function() {
+        $('span.highlight').contents().unwrap();
+        $('a.highlight').removeClass('highlight');
+      },
+      highlight: function(selection) {
+        if (selection.selectionText) {
+          // We've got selection text
+          $('body').highlight(selection.selectionText);
+        }
+        if (selection.linkUrl) {
+          // We've got a link
+          $('a[href="' + selection.linkUrl +'"]').addClass('highlight');
+        }
+      },
+      lookup: function() {
+        console.log('lookup');
+        chrome.runtime.sendMessage({type:'lookup',url:window.location.href}, function (response) {
+          console.log(response);
+          if (response.length) {
+            $('body').addClass('nts-content');
+            for(var i = 0; i < response.length; i++) {
+              NTS.Content.highlight(response[i]);
             }
-        };
-    }
+          }
+        });
+      },
+      getSelectionText: function() {
+        var html = "";
+
+        var sel = window.getSelection();
+        if (sel.rangeCount) {
+          var container = document.createElement("div");
+          for (var i = 0, len = sel.rangeCount; i < len; ++i) {
+            container.appendChild(sel.getRangeAt(i).cloneContents());
+          }
+
+        }
+
+        return container.innerText ? container.innerText : null;
+      },
+      setupKeyboardEventHandlers: function() {
+        var note, selection;
+        // Basic Keyboard Shortcut Implementation [Alt-C]
+        $(window).keyup(function(e) {
+          if (e.altKey && e.keyCode === 67) {
+            selection = NTS.Content.getSelectionText();
+            note = {
+              pageUrl: location.href,
+              context: 'context-page',
+              title: $('title').text()
+            }
+
+            if (selection) {
+              note.selectionText = selection;
+              note.context = 'context-selection';
+            }
+
+            chrome.runtime.sendMessage({
+              type:'createNote',
+              note: note
+            }, function (response) {
+              if (response.type === 'success') {
+                chrome.runtime.sendMessage({'type':'updateUI'});
+              }
+            });
+          }
+        });
+      }
+    };
+  }
 })(window);
--- src/js/lib/nts.js
+++ src/js/lib/nts.js
@@ -1,97 +1,129 @@
 (function (window, undefined) {
-    window.NTS = {
-        init: function() {
-            chrome.extension.onMessage.addListener(function(msg, sender, sendResponse) {
-                var allNotes, notes;
-                allNotes = JSON.parse(localStorage["nts"]);
-                switch (msg.type) {
-                    case "lookup": 
-                        console.log('lookup');
-                        sendResponse(_.where(allNotes,{'pageUrl': msg.url}));
-                    break;
-                    case "delete":
-                         notes = _.reject(allNotes, function(note) {
-                            return note.id === msg.noteId;
-                         });
-
-                         localStorage.nts = JSON.stringify(notes);
-                         sendResponse({"type":"success"});
-                         NTS.updateBadge();
-                    break;
-                    case "createNote":
-                        NTS.createNote(msg.note);
-                        sendResponse({"type":"success"});
-                    break;
-                    case "updateUI":
-                    console.log(sender);
-                        NTS.updateUI(sender.tab);
-                        sendResponse({"type": "success"});
-                    break;
-                }
+  window.NTS = {
+    init: function() {
+      chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
+        NTS.getNotes(function (allNotes) {
+          switch (msg.type) {
+            case "lookup":
+              var result = _.where(allNotes, {
+                pageUrl: msg.url
+              });
+              sendResponse(result);
+            break;
+            case "delete":
+              var notes = _.reject(allNotes, function (note) {
+              return note.id === msg.noteId;
             });
-
-            chrome.browserAction.onClicked.addListener(function(tab) {
-                NTS.navigateTo("feed.html");
+            chrome.storage.sync.set({'nts': notes}, function () {
+              sendResponse({"type":"success"});
             });
-            // Install time set-up
-            chrome.runtime.onInstalled.addListener(function() {
-                // Set up localStorage
-                if (!localStorage.nts) localStorage.nts = JSON.stringify([]);
-                
+            break;
+            case "createNote":
+              NTS.createNote(msg.note, function () {
+              sendResponse({"type":"success"});
             });
-            chrome.runtime.onStartup.addListener(function() {
-                NTS.setUpContextMenus();
-            });
+            break;
+            case "updateUI":
+              NTS.updateUI(sender.tab);
+            sendResponse({"type": "success"});
+            break;
+          }
+        });
+        return true;
+      });
 
-            NTS.setUpContextMenus();
-        },
-        getNotes: function () {
-            return JSON.parse(localStorage["nts"]);
-        },
-        clickHandler: function (info, tab) {
-            NTS.createNote(_.extend({title: tab.title, context: info.menuItemId}, info));
-            NTS.updateUI(tab);
-        },
-        createNote: function(note) {
-            var notes,
-                now = new Date();
-
-            note.id = now.getTime();
-            notes = NTS.getNotes();
-            notes.push(note);
-            localStorage["nts"] = JSON.stringify(notes);
-
-        },
-        updateUI: function(tab) {
-            console.log("UPDATE UI");
-            console.log(tab);
-
-            chrome.tabs.sendMessage(tab.id, {'type':'render'});
-            chrome.extension.sendMessage({'type':'render'});
-            NTS.updateBadge();
-        },
-        configureBadge: function () {
-            chrome.browserAction.setBadgeBackgroundColor({color: "#ff8000"});
-        },
-        updateBadge: function () {
-            var nts = NTS.getNotes();
-            if (nts.length > 0) {
-                chrome.browserAction.setBadgeText({"text": "" + nts.length + ""});
-            } else {
-                chrome.browserAction.setBadgeText({"text":""});
-            }
-        },
-        navigateTo: function (url) {
-            chrome.tabs.create({url:chrome.extension.getURL(url)}); 
-        },
-        setUpContextMenus: function () {
-            // Set up context menu tree
-            var contexts = ["page","selection","link","editable","image","video","audio"];
-            for (var i = 0; i < contexts.length; i++) {
-              var context = contexts[i];
-              var title = "Note '" + context + "' to self";
-              var id = chrome.contextMenus.create({"id": "context-" + context,"title": title, "contexts":[context], "onclick": NTS.clickHandler}); 
-            }
+      chrome.browserAction.onClicked.addListener(function(tab) {
+        NTS.navigateTo("feed.html");
+      });
+      // Install time set-up
+      chrome.runtime.onInstalled.addListener(function() {
+        // Set up localStorage
+        if (localStorage.nts) {
+          console.log('Existing legacy user, we need to migrate them to new storage.sync');
+          var localNts = JSON.parse(localStorage.nts);
+          chrome.storage.sync.set({'nts': localNts}, function () {
+            delete localStorage.nts;
+            console.log('DONE');
+          });
         }
+      });
+      chrome.runtime.onStartup.addListener(function() {
+        NTS.setUpContextMenus();
+      });
+
+      chrome.storage.onChanged.addListener(function () {
+        NTS.updateUI();
+        return true;
+      });
+
+      NTS.setUpContextMenus();
+      NTS.configureBadge();
+    },
+    getNotes: function (done) {
+      return chrome.storage.sync.get(function (store) {
+        done(store.nts);
+      });
+    },
+    clickHandler: function (info, tab) {
+      NTS.createNote(_.extend({title: tab.title, context: info.menuItemId}, info));
+    },
+    createNote: function(note, done) {
+      var done = done || function() {};
+      NTS.getNotes(function (notes) {
+        var now = new Date();
+        note.id = now.getTime();
+        notes.push(note);
+        chrome.storage.sync.set({'nts':notes}, function () {
+          done();
+        });
+      });
+
+    },
+    updateUI: function(tab) {
+      if (!tab) {
+        chrome.tabs.query({}, function (tabs) {
+          _.each(tabs, function (tab) {
+            chrome.tabs.sendMessage(tab.id, {'type':'render'});
+          });
+        });
+      } else {
+        chrome.tabs.sendMessage(tab.id, {'type':'render'});
+      }
+      chrome.runtime.sendMessage({'type':'render'});
+      NTS.updateBadge();
+    },
+    configureBadge: function () {
+      chrome.browserAction.setBadgeBackgroundColor({color: "#ff8000"});
+      chrome.storage.onChanged.addListener(function () {
+        NTS.updateBadge();
+        return true;
+      });
+    },
+    updateBadge: function () {
+      NTS.getNotes(function (nts) {
+        if (nts.length > 0) {
+          chrome.browserAction.setBadgeText({"text": "" + nts.length + ""});
+        } else {
+          chrome.browserAction.setBadgeText({"text":""});
+        }
+      });
+    },
+    navigateTo: function (url) {
+      chrome.tabs.create({url:chrome.extension.getURL(url)});
+    },
+    setUpContextMenus: function () {
+      // Set up context menu tree
+      var contexts = ["page","selection","link","editable","image","video","audio"];
+      for (var i = 0; i < contexts.length; i++) {
+        var context = contexts[i];
+        var title = "Note '" + context + "' to self";
+        var id = chrome.contextMenus.create({
+          "id": "context-" + context,
+          "title": title,
+          "contexts":[context],
+          "onclick": NTS.clickHandler
+        });
+      }
     }
+  }
 })(window);
